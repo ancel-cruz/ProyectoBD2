833b2da211cc1ccd67a4c7feeb91c65c283116f6
ancel-cruz <ancel.cruz@ucsp.edu.pe>
2022-07-04 20:05:41 -0500

    btree code

---
+++ xtable.h
@@ -0,0 +1,22 @@
+//
+// Created by Alain Cruz on 4/07/22.
+//
+
+#ifndef PROYECTOBD2_XTABLE_H
+#define PROYECTOBD2_XTABLE_H
+
+using namespace::std;
+
+enum types {type_int, type_float, type_double, type_bool, type_pchar};
+
+struct node{
+    types type;
+    void* vp;
+};
+
+struct table{
+    string name_table;
+    vector<node*>elements;
+};
+
+#endif //PROYECTOBD2_XTABLE_H
